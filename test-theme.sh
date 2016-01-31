05fce49bbd64f8e916fc5d6707606d268f8057d7
lbrfabio <lbrfabio@gmail.com>
2016-01-31 14:44:11 +0100

    support developer edition

--- test-theme.sh
+++ test-theme.sh
@@ -4,7 +4,7 @@ PROFILE=default
 
 ./make-xpi.py theme
 
-for directory in ~/.mozilla/firefox/*.${PROFILE}
+for directory in ~/.mozilla/firefox/*${PROFILE}
 do
     cp gnome-firefox-theme.xpi $directory/extensions/{451500c0-902c-11e0-91e4-0800200c9a66}.xpi
 done

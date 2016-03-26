981c1e4366bdfbf0c7ce7ed85d456a12934bd97b
lbrfabio <lbrfabio@gmail.com>
2016-03-26 15:24:28 +0100

    fix popup info icon

--- test-theme.sh
+++ test-theme.sh
@@ -9,4 +9,4 @@ do
     cp gnome-firefox-theme.xpi $directory/extensions/{451500c0-902c-11e0-91e4-0800200c9a66}.xpi
 done
 
-killall firefox firefox-bin &>/dev/null; firefox &
+killall firefox firefox-bin &>/dev/null; (firefox || firefox-dev) &
